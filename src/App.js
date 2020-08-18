4ac9eecc9c9df646bf8b06f7ca4c710d2cfd5bd1
Iskander <iskander.yuldashev@gmail.com>
2020-08-18 12:37:50 -0400

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Map from "./components/Map";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import UsersPage from './pages/UsersPage';
+import UserCard from './components/UserCard';
+
 function App() {
   return (
     <div className="App">
         <Router>
           <Switch>
+            <Route exact path="/" component={HomePage} />
             <Route exact path="/" component={Map} />
             <Route exact path="/users" component={UsersPage} />
             <Route exact path="/user/:userID" component={UserCard} />
