fec049b8bcd547b5c519c0c8752413e443e45c66
Iskander <iskander.yuldashev@gmail.com>
2020-08-18 11:56:42 -0400

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Map from "./components/Map";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Map />
+    <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Map} />
+            <Route exact path="/users" component={UsersPage} />
+            <Route exact path="/user/:userID" component={UserCard} />
+            <Redirect to="/" />
+          </Switch>
+        </Router>
     </div>
   );
-};
+}
 
 export default App;
